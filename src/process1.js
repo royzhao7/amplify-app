50e401a258201f0ec70fe0ee53940491ed97c64b
royzhao7 <zhaoshuai1106@gmail.com>
2023-02-28 10:09:16 +0800

    dynamic with bar length show subtitle

--- src/process1.js
+++ src/process1.js
@@ -1,12 +1,18 @@
 
 import { useState } from "react";
 import ReactApexChart from "react-apexcharts";
+import {Amplify,  API, graphqlOperation } from 'aws-amplify';
+import { onCreateGreengrassData } from './graphql/subscriptions';
+import awsExports from './aws-exports';
+
+
+Amplify.configure(awsExports);
 
 
 var state = {
     series: [{
         name: 'Process 1',
-        data: [44]
+        data: [0]
       }],
     options:{
           chart: {
@@ -38,13 +44,13 @@ var state = {
       floating: true,
       offsetX: -10,
       offsetY: 5,
-      text: 'Process 1'
+      text: 'Memory_BetaVM'
     },
     subtitle: {
       floating: true,
       align: 'right',
       offsetY: 0,
-      text: '44%',
+      text: '0%',
       style: {
         fontSize: '20px'
       }
@@ -78,12 +84,63 @@ var state = {
 function Process1() {
     const [chartdata,setchartdata]=useState(state.series);
 
+    const [chartsoptions,setchartoptions]=useState(state.options);
+
+
+
+
+    console.log('start subscription memory to sensor');
+
+    const subscriber = API.graphql(graphqlOperation(onCreateGreengrassData)).subscribe({
+      next: (response) => {
+
+        //update the sensor's status in state
+        if (response) {
+          console.log(response.value.data);
+        //  setSensorValue(response.value.data.onCreateGreengrassData.greengrass_data)
+         var memoryCount=Math.round(JSON.parse(response.value.data.onCreateGreengrassData.greengrass_data)[2].V);
+        
+         const randomCpuLoad2 = Math.floor(Math.random() * 100)+1;
+
+         const memoryUsage=Math.floor((memoryCount/3901)* 100)
+
+         console.log("memoryUsage: "+memoryUsage);
+        
+       
+
+
+// console.log("options: "+state.options.subtitle.text);
+
+         setchartdata([{
+          name: 'Process 1',
+          data: [memoryUsage]
+        }]);
+
+        const memorySubtitleText=memoryUsage+'%';
+        setchartoptions((prev) => ({
+          ...prev,
+          subtitle: {
+            floating: true,
+      align: 'right',
+      offsetY: 0,
+      text: memorySubtitleText,
+      style: {
+        fontSize: '20px'
+      }
+          },
+        }));
+        }
+      },
+      error: (error) => {
+        console.log('error on sensor subscription', error);
+      }
+    }); 
 
 
     
   return (
       <ReactApexChart
-        options={state.options}
+        options={chartsoptions}
         series={chartdata}
         type="bar"
         height={70}
